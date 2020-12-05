c3073d1ca3593ebca85e585e6f53f36f1a666e94
vanessa <vamdc.adm@gmail.com>
2020-12-05 18:19:57 -0300

    improve login and sign up design+tweaks

--- project/main.py
+++ project/main.py
@@ -11,4 +11,4 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-  return render_template('profile.html', name = current_user.name)
+  return render_template('profile.html', firstname = current_user.firstname, lastname = current_user.lastname)
