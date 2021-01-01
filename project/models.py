12ab89b759c1e5547400e018264126fdf928d512
vanessa <vamdc.adm@gmail.com>
2021-01-01 10:46:05 -0300

    fix-facebook-error

--- project/models.py
+++ project/models.py
@@ -14,7 +14,7 @@ class User(UserMixin, db.Model):
 class OAuth(OAuthConsumerMixin, db.Model):
   __table_args__ = (db.UniqueConstraint("provider", "provider_user_id"),)
   provider_user_id = db.Column(db.String(256), nullable = False)
-  provider_user_login = db.Column(db.String(256), nullable = False)
+  provider_user_login = db.Column(db.String(256))
   user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable = False)
   user = db.relationship(User)
 
